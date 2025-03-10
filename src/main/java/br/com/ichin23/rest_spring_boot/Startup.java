a8abbcdde6653e9d1eaa76a7bdf02ad8f9e887fd
ichin23 <pedroferreiramanoel@gmail.com>
2025-03-10 07:57:12 -0300

    first route

---
+++ src/main/java/br/com/ichin23/rest_spring_boot/Startup.java
@@ -0,0 +1,13 @@
+package br.com.ichin23.rest_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ichin23/rest_spring_boot/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.ichin23.rest_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
